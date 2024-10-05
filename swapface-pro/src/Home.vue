0d5c02ad50254dff43330c5fbff6bb0c6b384d5c
Bbypepe <babypepeofficially@gmail.com>
2024-10-05 02:37:46 +0000

    Initial commit for mynewface project

---
+++ swapface-pro/src/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Swapface Pro</h1>
+    <router-link to="/settings">Go to Settings</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ swapface-pro/src/Settings.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="settings">
+    <h2>Settings</h2>
+    <p>Manage your application settings here.</p>
+    <router-link to="/">Go back to Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Settings'
+}
+</script>
+
+<style scoped>
+h2 {
+  color: #ff6347;
+}
+</style>
